ac7dc359ab3f538efcf7ad01a2ee7d124b5ac8e7
fxf981 <yeyunet@gmail.com>
2024-04-10 16:37:13 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,27 +1,27 @@
-// @ts-nocheck
+/**
+ * Welcome to Cloudflare Pages! This is your first page.
+ *
+ * - Run "npm run dev" in your terminal to start a development server
+ * - Open a browser tab at http://localhost:8787/ to see your page in action
+ * - Run "npm run deploy" to publish your page
+ *
+ * Learn more at https://developers.cloudflare.com/pages/
+ */
 
-addEventListener('fetch', event => {
-    event.respondWith(handleRequest(event.request));
-  });
-  
-  async function handleRequest(request) {
+const tg_host = "api.telegram.org";
+
+export default {
+  async fetch(request, env, ctx) {
     const url = new URL(request.url);
-    url.host = 'api.telegram.org'; // Forward all requests to api.telegram.org
-  
-    const modifiedRequest = new Request(url.toString(), {
+    url.host = tg_host;
+
+    const proxyRequest = new Request(url, {
       method: request.method,
       headers: request.headers,
       body: request.body,
     });
-  
-    try {
-      const response = await fetch(modifiedRequest);
-      return response;
-    } catch (error) {
-      return new Response(JSON.stringify({ error: error.message }), {
-        status: 500,
-        headers: { 'Content-Type': 'application/json' },
-      });
-    }
-  }
-  
+
+    const result = await fetch(proxyRequest);
+    return result;
+  },
+};
