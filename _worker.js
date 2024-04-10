a22676e92a24d0261103265ccaffade2bced8f4c
fxf981 <105899270+fxf981@users.noreply.github.com>
2024-04-10 14:45:33 +0800

    增加domain变量

--- _worker.js
+++ _worker.js
@@ -1,20 +1,11 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run "npm run dev" in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run "npm run deploy" to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
-const tg_host = "api.telegram.org";
+let domain = "api.telegram.org";
+domain = env.domain || domain;
 addEventListener('fetch', event => {
     event.respondWith(handleRequest(event.request))
 })
 async function handleRequest(request) {
     var u = new URL(request.url);
-    u.host = tg_host;
+    u.host = domain;
     var req = new Request(u, {
         method: request.method,
         headers: request.headers,
