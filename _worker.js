ec27a223dbb8ccc0debcd6ba02fe8974cda06f86
fxf981 <yeyunet@gmail.com>
2024-04-11 10:00:55 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -8,13 +8,38 @@
  * Learn more at https://developers.cloudflare.com/pages/
  */
 
-const tg_host = "api.telegram.org";
+// const tg_host = "api.telegram.org";
+// export default {
+//   async fetch(request, env, ctx) {
+//     const url = new URL(request.url);
+//     url.host = tg_host;
+
+//     const proxyRequest = new Request(url, {
+//       method: request.method,
+//       headers: request.headers,
+//       body: request.body,
+//     });
+
+//     const result = await fetch(proxyRequest);
+//     return result;
+//   },
+// };
+/**
+ * Welcome to Cloudflare Pages! This is your first page.
+ *
+ * - Run "npm run dev" in your terminal to start a development server
+ * - Open a browser tab at http://localhost:8787/ to see your page in action
+ * - Run "npm run deploy" to publish your page
+ *
+ * Learn more at https://developers.cloudflare.com/pages/
+ */
+
 export default {
   async fetch(request, env, ctx) {
     const url = new URL(request.url);
-    url.host = tg_host;
+    const tg_host = url.searchParams.get('tg_host') || 'api.telegram.org';
 
-    const proxyRequest = new Request(url, {
+    const proxyRequest = new Request(`https://${tg_host}${url.pathname}`, {
       method: request.method,
       headers: request.headers,
       body: request.body,
